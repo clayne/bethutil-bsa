9fe9d98a8beed661a56c9d755e05dd79d067c09f
G'k <39066502+Guekka@users.noreply.github.com>
2021-08-25 18:30:13 +0200

    Remove unused functions

--- include/btu/bsa/detail/archive.hpp
+++ include/btu/bsa/detail/archive.hpp
@@ -58,9 +58,6 @@ public:
     size_t add_file(const std::filesystem::path &a_root, const std::filesystem::path &a_path);
     size_t add_file(const std::filesystem::path &a_relative, std::vector<std::byte> a_data);
 
-    void merge(Archive other);
-    size_t decompress();
-
     using iteration_callback = std::function<void(const std::filesystem::path &, std::span<const std::byte>)>;
     void iterate_files(const iteration_callback &a_callback, bool skip_compressed = false);
 
